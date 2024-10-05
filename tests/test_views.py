e304f72f9d059b9bdcd4c663a077cf83edebe72e
bv <bv@subtotal.ru>
2024-10-06 01:14:53 +0300

    first commit

---
+++ tests/test_views.py
@@ -0,0 +1,13 @@
+from test_pyramid.views.default import my_view
+from test_pyramid.views.notfound import notfound_view
+
+
+def test_my_view(app_request):
+    info = my_view(app_request)
+    assert app_request.response.status_int == 200
+    assert info['project'] == 'test-pyramid'
+
+def test_notfound_view(app_request):
+    info = notfound_view(app_request)
+    assert app_request.response.status_int == 404
+    assert info == {}
