17c18feef64bc6ddc0034e9a9534492a1790dabf
bv <bv@subtotal.ru>
2024-10-06 01:30:06 +0300

    second commit

--- test_pyramid/views/default.py
+++ test_pyramid/views/default.py
@@ -1,6 +1,13 @@
 from pyramid.view import view_config
+from pyramid.response import Response
 
 
 @view_config(route_name='home', renderer='test_pyramid:templates/mytemplate.jinja2')
 def my_view(request):
     return {'project': 'test-pyramid'}
+
+
+@view_config(route_name='hello')
+def hello_world(request):
+    body = '<h1>Hello everyone!</h1>'
+    return Response(body)
